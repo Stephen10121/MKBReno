5aad7128fa13f3f1d3668509bbb6ad1e90095fed
Stephen Gruzin <stephengrzn@gmail.com>
2026-03-06 00:47:55 -0800

    adding back the carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import Hero from "@/components/homePage/Hero.svelte";
-    import ImageCarousel from "@/components/homePage/ImageCarousel.svelte";
+    // import ImageCarousel from "@/components/homePage/ImageCarousel.svelte";
     import Reviews from "@/components/homePage/Reviews.svelte";
     import Services from "@/components/homePage/Services.svelte";
 </script>
